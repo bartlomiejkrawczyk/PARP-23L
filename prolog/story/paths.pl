f7884f8a6a2b847948149ee183f03a5e48c24a6c
Jakub Winter <01158986@pw.edu.pl>
2023-03-11 22:58:29 +0100

    Add forest paths

--- prolog/story/paths.pl
+++ prolog/story/paths.pl
@@ -10,8 +10,13 @@ two_way_path(city_square, s, police_station).
 two_way_path(city_square, e, westbourne_road).
 two_way_path(westbourne_road, n, museum).
 two_way_path(westbourne_road, e, forest).
-two_way_path(forest, e, forest_path).
+
+% Forest
 two_way_path(forest, n, forest).
+two_way_path(forest, e, forest_path).
+two_way_path(forest_path, e, forest_glade). % TODO: powinno być ustawiane przy rozmowie z tomem
+two_way_path(forest_glade, n, toms_house).
+two_way_path(forest_glade, s, old_tree). % TODO: powinno być ustawiane przy rozmowie z dziećmi
 
 % Museum
 two_way_path(museum, n, reception).
@@ -41,6 +46,32 @@ locked_two_way_path(greek_exhibit_hall, w, janitors_closet, old_key).
 locked_two_way_path(greek_exhibit_hall, e, cctv_room, key_card).
 locked_two_way_path(roman_exhibit_hall, n, renovated_exhibit, key).
 
+locked_two_way_path(old_tree, e, digging_marks, shovel).
+
+% One-way forest roads
+% path(forest_glade, s, forest). % TODO: powinno być usuwane przy rozmowie z dziećmi
+% path(forest_path, e, forest).  % TODO: powinno być usuwane przy rozmowie z tomem
+
+path(forest_glade, e, forest).
+
+path(old_tree, s, forest).
+path(old_tree, w, forest).
+
+path(forest_path, n, forest).
+path(forest_path, s, forest).
+
+path(Here, Direction, There) :-
+        locked_path(Here, Direction, There, Object),
+        holding(Object),
+        !.
+
+/* These rules define the possible movement in two directions */
+
+path(Here, Direction, There) :- two_way_path(Here, Direction, There).
+path(Here, Direction, There) :-
+    opposite_directions(Direction, OppositeDirection),
+    two_way_path(There, OppositeDirection, Here).
+
 /* These rules define the locked doors mechanic */
 
 locked_path(Here, Direction, There, Object) :- locked_two_way_path(Here, Direction, There, Object).
@@ -49,21 +80,6 @@ locked_path(Here, Direction, There, Object) :-
         opposite_directions(Direction, OppositeDirection),
         locked_two_way_path(There, OppositeDirection, Here, Object), !.
 
-path(Here, Direction, There) :-
-            locked_path(Here, Direction, There, Object),
-            holding(Object),
-            !.
-
-/* These rules define the one way paths positioning */
-
-path(someplace1, e, someplace3).
-
-/* These rules define the possible movement in two directions */
-
-path(Here, Direction, There) :- two_way_path(Here, Direction, There).
-path(Here, Direction, There) :-
-        opposite_directions(Direction, OppositeDirection),
-        two_way_path(There, OppositeDirection, Here).
 
 /* These rules define the opposite world directions */
 
