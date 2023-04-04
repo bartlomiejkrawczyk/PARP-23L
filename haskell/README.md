f4be291fb85888f7d59726caf931ed0fad69f8ba
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-04-04 22:19:52 +0200

    Configure basic haskell project and add instruction highlighting

--- haskell/README.md
+++ haskell/README.md
@@ -1,4 +1,4 @@
-# Haskel
+# Haskell
 
 ## Problem
 
--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,25 +1,10 @@
--- The germ of a text adventure game
--- Marcin Szlenk 2022
+module Main where
+
+import System.IO
+import Rules.Colors
 
 introductionText = [
-    "A long time ago, in a galaxy far, far away...",
-    "",
-    "It is a period of civil war. Rebel",
-    "spaceships, striking from a hidden",
-    "base, have won their first victory",
-    "against the evil Galactic Empire.",
-    "",
-    "During the battle, Rebel spies managed",
-    "to steal secret plans to the Empire's",
-    "ultimate weapon, the Death Star, an",
-    "armored space station with enough",
-    "power to destroy an entire planet.",
-    "",
-    "Pursued by the Empire's sinister agents,",
-    "Princess Leia races home aboard her",
-    "starship, custodian of the stolen plans",
-    "that can save her people and restore",
-    "freedom to the galaxy....",
+    "TODO",
     ""
     ]
 
@@ -40,8 +25,10 @@ printInstructions = printLines instructionsText
 
 readCommand :: IO String
 readCommand = do
-    putStr "> "
+    putStr (colorGreen ++ "> ")
+    hFlush stdout
     xs <- getLine
+    putStr colorDefault
     return xs
     
 -- note that the game loop may take the game state as
