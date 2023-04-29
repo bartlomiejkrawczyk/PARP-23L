34f8bc2ecca9e517bf02895abea0ecf2d0c1d4a4
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-04-29 15:17:58 +0200

    Update haskell project

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,50 +1,52 @@
 module Main where
 
-import System.IO
 import Rules.Colors
+import System.IO
 
-introductionText = [
-    "TODO",
+introductionText =
+  [ "TODO",
     ""
-    ]
+  ]
 
-instructionsText = [
-    "Available commands are:",
+instructionsText =
+  [ "Available commands are:",
     "",
     "instructions  -- to see these instructions.",
     "quit          -- to end the game and quit.",
     ""
-    ]
+  ]
 
 -- print strings from list in separate lines
 printLines :: [String] -> IO ()
 printLines xs = putStr (unlines xs)
-                  
+
 printIntroduction = printLines introductionText
+
 printInstructions = printLines instructionsText
 
 readCommand :: IO String
 readCommand = do
-    putStr (colorGreen ++ "> ")
-    hFlush stdout
-    xs <- getLine
-    putStr colorDefault
-    return xs
-    
+  putStr (colorGreen ++ "> ")
+  hFlush stdout
+  xs <- getLine
+  putStr colorDefault
+  return xs
+
 -- note that the game loop may take the game state as
 -- an argument, eg. gameLoop :: State -> IO ()
 gameLoop :: IO ()
 gameLoop = do
-    cmd <- readCommand
-    case cmd of
-        "instructions" -> do printInstructions
-                             gameLoop
-        "quit" -> return ()
-        _ -> do printLines ["Unknown command.", ""]
-                gameLoop
+  cmd <- readCommand
+  case cmd of
+    "instructions" -> do
+      printInstructions
+      gameLoop
+    "quit" -> return ()
+    _ -> do
+      printLines ["Unknown command.", ""]
+      gameLoop
 
 main = do
-    printIntroduction
-    printInstructions
-    gameLoop
-
+  printIntroduction
+  printInstructions
+  gameLoop
